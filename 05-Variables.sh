976e618901b339f7c2e01e2c5683e300a51acc99
Syed <shahabsb@amazon.com>
2024-12-31 16:52:55 +0530

    conversation added

--- 05-Variables.sh
+++ 05-Variables.sh
@@ -3,7 +3,7 @@
 echo "Please enter your name ?"
 read -s USERNAME
 
-echo "Username entered is : $USERNAME"
+echo "username entered is : $USERNAME"
 
-echo "Please enter your Password"
+echo "Please enter your Password :"
 read -s PASSWORD
